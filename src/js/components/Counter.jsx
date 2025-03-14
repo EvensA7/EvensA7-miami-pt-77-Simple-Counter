f7fede101830de2628f2d192d0754846b39fe3c4
Evens Antoine <evens7antoine@gmail.com>
2025-03-14 21:35:42 +0000

    Done

--- src/js/components/Counter.jsx
+++ src/js/components/Counter.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 const Counter = (props) => {
+    return (
+        <div className="text-center d-flex justify-content-center">
+            <p>{Math.floor(props.seconds/10000) % 10}</p>
+            <p>{Math.floor(props.seconds/1000) % 10}</p>
+            <p>{Math.floor(props.seconds/100) % 10}</p>
+            <p>{Math.floor(props.seconds/10) % 10}</p>
+            <p>{Math.floor(props.seconds/1) % 10}</p>
+        </div>
 
+    );
     
-}
+};
+
+export default Counter;
